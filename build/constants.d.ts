87197658c255135341c6b08a45623e3112312c65
Hyunggyu Jang <hyunggyu.jang@42dot.ai>
2024-12-11 15:18:50 +0900

    feat: Generate type declarations while building

---
+++ build/constants.d.ts
@@ -0,0 +1,5 @@
+export declare const AGGREGATE_CHECKS = true;
+export declare const EXIT_ON_ERROR = false;
+export declare const LOG_FAILURES = false;
+export declare const TRUNCATE_VARIABLE_THRESHOLD = 100;
+export declare const TRUNCATE_MSG_THRESHOLD = 300;
